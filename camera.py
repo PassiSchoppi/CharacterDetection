613aed6767802d7632b9f5f73ae96d51cef465dd
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2019-04-13 12:04:15 +0200

    fliping image; border detection
    image processing with OpenCV
    	modified:   camera.py

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 cv2.namedWindow("preview")
@@ -12,7 +13,24 @@ else:
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
+
+    # percent of original size
+    scale_percent = 150
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    
+    # flip image
+    frame = cv2.flip(frame, 1)
+
+    # show borders
+    frame = cv2.Canny(frame, 200,200)
+
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
 cv2.destroyWindow("preview")
+
