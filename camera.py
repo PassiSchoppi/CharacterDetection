5f4e52aee00a72b00f1ce8e7438b4c77e7b15dd4
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2019-04-14 21:20:56 +0200

    camera.py records and saves video from webcam
    with camera.py you can save all the pre processed data from your webcam
    to a .pickle file that later can be read in to an array
    	modified:   camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
-import numpy as np
 import cv2
+import pickle
 
 cv2.namedWindow("preview")
 # change folowing number if its not the main camera
@@ -10,16 +10,17 @@ if vc.isOpened(): # try to get the first frame
 else:
     rval = False
 
+im_array = []
+
 while rval:
-    cv2.imshow("preview", frame)
+    # read from camera
     rval, frame = vc.read()
 
     # percent of original size
-    scale_percent = 150
+    scale_percent = 30
     width = int(frame.shape[1] * scale_percent / 100)
     height = int(frame.shape[0] * scale_percent / 100)
     dim = (width, height)
-
     # resize image
     frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
     
@@ -29,8 +30,29 @@ while rval:
     # show borders
     frame = cv2.Canny(frame, 200,200)
 
+    # append to array
+    im_array.append(frame)
+    
+    # resize image
+    scale_percent = 450
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+
+    # show
+    cv2.imshow("preview", frame)
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+
+# save out
+# save as .gif
+# nononononono
+# save as .pickle
+pickle_out = open("im_array_train.pickle","wb")
+pickle.dump(im_array, pickle_out)
+pickle_out.close()
+
 cv2.destroyWindow("preview")
 
